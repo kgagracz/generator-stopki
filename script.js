68047fe53705597d8d34b2843909649c895f652a
kgagracz <krystian.gagracz@gmail.com>
2021-06-14 15:27:42 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let nameInput = document.getElementById('name');
+let nameOutput = document.getElementById('name-output');
+
+let positionInput = document.getElementById('position');
+let positionOutput = document.getElementById('position-output');
+
+let phoneInput = document.getElementById('phone');
+let phoneOutput = document.getElementById('phone-output');
+console.log(phoneOutput)
+let emailInput = document.getElementById('email');
+let emailOutput = document.getElementById('email-output');
+
+let imgUrlInput = document.getElementById('image-url');
+let imgUrlOutput = document.getElementById('image-url-output');
+
+let submitButton = document.getElementById('submit');
+
+function generate() {
+    nameOutput.innerHTML = nameInput.value;
+    positionOutput.innerHTML = positionInput.value;
+    phoneOutput.innerHTML = phoneInput.value;
+    emailOutput.innerHTML = emailInput.value;
+    imgUrlOutput.src = imgUrlInput.value;
+
+}
+
+submitButton.addEventListener('click', generate);
