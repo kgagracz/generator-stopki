5125eec77001953007c956fa691f6cea4b3d8130
kgagracz <krystian.gagracz@gmail.com>
2021-06-15 09:07:17 +0200

    displaying code on screen

--- script.js
+++ script.js
@@ -6,27 +6,35 @@ let positionOutput = document.getElementById('position-output');
 
 let phoneInput = document.getElementById('phone');
 let phoneOutput = document.getElementById('phone-output');
-console.log(phoneOutput)
 let emailInput = document.getElementById('email');
 let emailOutput = document.getElementById('email-output');
-
 let imgUrlInput = document.getElementById('image-url');
 let imgUrlOutput = document.getElementById('image-url-output');
 
+let fbLinkInput = document.getElementById('fb-link');
+let fbLinkOutput = document.getElementById('fb-link-output');
+let igLinkInput = document.getElementById('ig-link');
+let igLinkOutput = document.getElementById('ig-link-output');
+let liLinkInput = document.getElementById('li-link');
+let liLinkOutput = document.getElementById('li-link-output');
+
 let submitButton = document.getElementById('submit');
 
-let codeOutput = document.getElementById('signature');
+let codeOutput = document.getElementById('code');
 function generate() {
     nameOutput.innerHTML = nameInput.value;
     positionOutput.innerHTML = positionInput.value;
     phoneOutput.innerHTML = phoneInput.value;
-    emailOutput.innerHTML = emailInput.value;
+    emailOutput.href = emailInput.value;
     imgUrlOutput.src = imgUrlInput.value;
+    fbLinkOutput.href = fbLinkInput.value;
+    igLinkOutput.href = igLinkInput.value;
+    liLinkOutput.href = liLinkInput.value;
     
-    
-    let codeToHighlight = document.getElementById('Tabela_01');
-    codeOutput.innerHTML = codeToHighlight.outerHTML
-    
+    let codeToHighlight = '';
+    codeToHighlight = document.getElementById('Tabela_01').outerHTML;
+    codeOutput.innerHTML = codeToHighlight
+    console.log(codeToHighlight)
 
 }
 
